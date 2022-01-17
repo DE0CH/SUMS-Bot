c62df409e1b98438894aecc402bb1a7e91902ec9
Deyao Chen <chendeyao000@gmail.com>
2022-01-18 04:47:58 +0800

    remove db backup

--- keep_alive.py
+++ keep_alive.py
@@ -9,20 +9,7 @@ app = Flask('')
 def home():
     return "EmailBot is running"
 
-def progress(status, remaining, total):
-    print(f'Copied {total-remaining} of {total} pages...')
-
-def backup_db():
-    con = sqlite3.connect('bot.db')
-    bck = sqlite3.connect('backup.db')
-    with bck:
-        con.backup(bck, pages=1, progress=progress)
-    bck.close()
-    con.close()
-    print("Sqlite3 backed up")
-
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(backup_db,'interval',minutes=30)
 sched.start()
 
 def run():
